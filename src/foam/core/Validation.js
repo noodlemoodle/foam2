9dc67ec8e2722c77de13ac6dccbdbdacc7fa5935
noodlemoodle <qiyzhang@icloud.com>
2020-05-27 11:27:32 -0400

    add documentation to errorMessage

--- src/foam/core/Validation.js
+++ src/foam/core/Validation.js
@@ -39,11 +39,20 @@ foam.CLASS({
     },
     {
       class: 'String',
-      name: 'errorMessage'
+      name: 'errorMessage',
+      documentation: `
+      Provide feedback to the user via a Message.
+      To use this, provide the name of the Message you wish to add.
+      When both errorString and errorMessage are specified, the errorMessage will be used.
+      `
     },
     {
       class: 'String',
-      name: 'errorString'
+      name: 'errorString',
+      documentation: `
+      Provide feedback to the user via a String.
+      When both errorString and errorMessage are specified, the errorMessage will be used.
+      `
     },
     {
       class: 'Function',
